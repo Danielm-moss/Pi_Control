88367f1c4f6b8af6cc1ef150521c4131038b1a09
Danielm-moss <daniel@moss.ag>
2025-12-18 19:34:39 -0500

    Update Pi-FrameCapture.sh

--- Pi-FrameCapture.sh
+++ Pi-FrameCapture.sh
@@ -4,18 +4,20 @@ mkdir -p "$OUTDIR"
 
 echo 1 | sudo tee /sys/module/imx296/parameters/trigger_mode >/dev/null # Disable hardware trigger mode
 
-EXPOSURES=(100 200 300 400 500 600 700 800 900 1000) # Exposure times in microseconds
-GAIN=5
+# user assigned variables
+exposures=(100 200 300 400 500 600 700 800 900 1000) # Exposure times in microseconds
+gain=5
+# end of user assigned variables
 
-time_array_size=${#EXPOSURES[@]}
-current_time=0 # Index into EXPOSURES array
+time_array_size=${#exposures[@]}
+current_time=0 # Index into exposures array
 
 # Capture loop: cycles through exposure times
 while [ "$current_time" -lt "$time_array_size" ]; do
-    exp_us=${EXPOSURES[$current_time]}
+    exp_us=${exposures[$current_time]}
 
-    filename="$OUTDIR/T_${exp_us}_G_${GAIN}.jpg"
-    rpicam-still --immediate -n --shutter "$exp_us" --gain "$GAIN" --awb auto -o "$filename"
+    filename="$OUTDIR/T_${exp_us}_G_${gain}.jpg"
+    rpicam-still --immediate -n --shutter "$exp_us" --gain "$gain" --awb auto -o "$filename"
     echo "Captured frame: $filename"
 
     current_time=$((current_time + 1))
